490aa3d33b7a0f9eea852c26d60940ca1372f64d
sanjayram21 <sanjayram21@gmail.com>
2022-04-14 16:52:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Hey !
+
+This is a simple chat application built using node.js and socket.io.
+
+First install socket.io by running 'npm i socket.io'
+Install nodemon by running 'npm i --save-dev nodemon'
+
+To run the application type in, 'npm run devStart' in the terminal.
+
+Open multiple application to chat with each other.
+
+Enjoy !
---
+++ server.js
@@ -0,0 +1,24 @@
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user", (username) => {
+    users[socket.id] = username;
+    socket.broadcast.emit("user-connected", username);
+  });
+  socket.on("send-chat-message", (message) => {
+    socket.broadcast.emit("chat-message", {
+      message: message,
+      username: users[socket.id],
+    });
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
+  });
+});
